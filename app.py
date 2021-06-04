0fb740982fd9a9de3313058568767ba56786ecf0
Drammaster <Csabi1996@windowslive.com>
2021-06-04 14:43:07 +1200

    Bot update patch

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json, config
-
 import requests
-from math import floor
 from flask import Flask, request, render_template
 from binance.client import Client
 from binance.enums import *
 
+
 app = Flask(__name__)
+
 
 trading_bots = [
     {
@@ -14,24 +14,28 @@ trading_bots = [
         'exchange_pair': "CAKEBUSD",
         'crypto': 'CAKE',
         'hold': 23,
-        'holds': False,
-        'initial': 200,
+        'holds': True,
+        'value': 200,
         'profit': 0,
         '7days': 0,
+        'order_type': ORDER_TYPE_MARKET
     },
     {
         'name': "Bitcoin Bot",
         'exchange_pair': "BTCBUSD",
         'crypto': 'BTC',
         'hold': 77,
-        'holds': False,
-        'initial': 800,
+        'holds': True,
+        'value': 800,
         'profit': 0,
         '7days': 0,
+        'order_type': ORDER_TYPE_MARKET
     }
 ]
 
+
 client = Client(config.API_KEY, config.API_SECRET)
+
 
 def add_bot(new_bot):
 
@@ -61,7 +65,7 @@ def add_bot(new_bot):
 
 
 # Live trading function
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+def order_function(side, quantity, symbol, order_type):
     try:
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
         order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
@@ -71,33 +75,45 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
 
     return order
 
+
 # Test trading function
-def testorder(side, quantitytest, symbol, order_type=ORDER_TYPE_MARKET):
+def test_order_function(side, quantity, symbol, order_type):
     try:
-        print(f"sending order {order_type} - {side} {quantitytest} {symbol}")
-        order = client.create_test_order(symbol=symbol, side=side, type=order_type, quantity=quantitytest)
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_test_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
 
     return order
 
+
 # Home page
 @app.route('/')
 def welcome():
     balances = client.get_account()['balances']
 
-    for i in trading_bots:
-        price = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + i['exchange_pair']).json()
-        i['profit'] = i['initial'] - (float(price['price']) * float(client.get_asset_balance(asset=i['crypto'])['free']))
+    return render_template('index.html', balances=balances)
 
-    return render_template('index.html', balances=balances, bots=trading_bots)
+
+@app.route('/bots')
+def show_bots():
+
+    for i in trading_bots:
+        pair_price = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + i['exchange_pair']).json()
+        token_holding = client.get_asset_balance(asset=i['crypto'])
+        i['value'] = round(float(pair_price['price']) * float(token_holding['free']), 2)
+
+    return render_template('bots.html', bots=trading_bots)
+
 
 # Live trade webhook
-@app.route('/webhook', methods=['POST'])
-def webhook():
+@app.route('/order', methods=['POST'])
+def order():
     # Load data from post
     data = json.loads(request.data)
+
+    order_type = ORDER_TYPE_MARKET
     
     # Check for security phrase
     if data['passphrase'] != config.WEBHOOK_PHRASE:
@@ -134,17 +150,17 @@ def webhook():
     exchange = data['ticker']
     if data['ticker'] == 'CAKEBUSD':
         if quantity > 0:
-            order_response = order(side, round(quantity - 0.001, 3), exchange)
+            order_response = order_function(side, round(quantity - 0.001, 3), exchange, order_type)
         else:
             order_response = True
     elif data['ticker'] == 'BTCBUSD':
         if quantity > 0:
-            order_response = order(side, round(quantity - 0.000001, 6), exchange)
+            order_response = order_function(side, round(quantity - 0.000001, 6), exchange, order_type)
         else:
             order_response = True
     elif data['ticker'] == 'BNBBUSD':
         if quantity > 0:
-            order_response = order(side, round(quantity - 0.0001, 4), exchange)
+            order_response = order_function(side, round(quantity - 0.0001, 4), exchange, order_type)
         else:
             order_response = True
 
@@ -155,98 +171,132 @@ def webhook():
         }
     else:
         if float(client.get_asset_balance(asset="BUSD")['free']) > 10:
-            webhook()
+            order()
         else:
             return {
                 "code": "error",
                 "message": "not enought funds"
             }
 
-# Test trade webhook
-@app.route('/test', methods=['POST'])
-def test():
-    data = json.loads(request.data)
-    
-    if data['passphrase'] != config.WEBHOOK_PHRASE:
-        return {
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
 
-    side = data['strategy']['order_action'].upper()
-    quantity = data['strategy']['order_contracts']
-    exchange = data['ticker']
-    order_response = testorder(side, quantity, exchange)
-
-    if order_response:
-        return {
-            "code": "success",
-            "message": "order executed"
-        }
-    else:
-        print("order failed")
-
-        return {
-            "code": "error",
-            "message": "order failed"
-        }
-
-@app.route('/set', methods=['POST'])
-def initial():
+@app.route('/ordertest', methods=['POST'])
+def ordertest():
     # Load data from post
     data = json.loads(request.data)
 
+    order_type = ORDER_TYPE_MARKET
+    
     # Check for security phrase
     if data['passphrase'] != config.WEBHOOK_PHRASE:
         return {
             "code": "error",
             "message": "Nice try, invalid passphrase"
         }
-    
+
+    # Check for buy or for sell
     side = data['strategy']['order_action'].upper()
 
     
     # Buy case
     if side == "BUY":
-        allowence = 100
+        allowence = 0
         for i in trading_bots:
-            if i['exchange_pair'] != data['ticker'] and i['holds'] == True:
-                allowence -= i['hold']
-        assets = client.get_asset_balance(asset="BUSD")
-        price = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + data['ticker']).json()
-        quantity = float(((float(assets['free'])*(allowence/100)) / float(price['price']))*0.9995)
-    
-    return({
-        'Status': client.get_account_status(),
-        'Snapshot': client.get_account_snapshot(),
-        'Fees': client.get_my_trades(symbol="CAKEBUSD")
-        })
+            if i['exchange_pair'] == data['ticker'] and i['holds'] == False:
+                allowence += i['hold']
+                i['holds'] = True
+            assets = client.get_asset_balance(asset="BUSD")
+            price = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + data['ticker']).json()
+            quantity = float(((float(assets['free'])*(allowence/100)) / float(price['price']))*0.9995)
 
-# Home page
+
+    # Sell case
+    elif side == "SELL":
+        for i in trading_bots:
+            if i['exchange_pair'] == data['ticker']:
+                i['holds'] = False
+        assets = client.get_asset_balance(asset=data['crypto'])
+        quantity = float(assets['free'])
+
+
+    exchange = data['ticker']
+    if data['ticker'] == 'CAKEBUSD':
+        if quantity > 0:
+            order_response = test_order_function(side, round(quantity - 0.001, 3), exchange, order_type)
+        else:
+            order_response = True
+    elif data['ticker'] == 'BTCBUSD':
+        if quantity > 0:
+            order_response = test_order_function(side, round(quantity - 0.000001, 6), exchange, order_type)
+        else:
+            order_response = True
+    elif data['ticker'] == 'BNBBUSD':
+        if quantity > 0:
+            order_response = test_order_function(side, round(quantity - 0.0001, 4), exchange, order_type)
+        else:
+            order_response = True
+
+
+    if order_response == {}:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    elif order_response:
+        return {
+            "code": "error",
+            "message": "Bot already traded in"
+        }
+
+
+# Parallax page
 @app.route('/parallax')
 def parallax():
     return render_template('parallax.html')
 
 
-@app.route('/createbot', methods=['POST'])
-def create_bot():
+@app.route('/managebot', methods=['POST'])
+def manage_bot():
     # Load data from post
     data = json.loads(request.data)
 
-    name = data['name']
-    exchange_pair = data['exchange_pair']
-    hold = data['hold']
 
-    new_bot = {
-        'name': name,
-        'exchange_pair': exchange_pair,
-        'hold': hold,
-        'holds': False
-    }
+    # Check request
+    if data['task'] == 'update':
+        allowance = 100
+        for i in trading_bots:
+            if i['name'] == data['name']:
+                i['hold'] = data['hold']
+                allowance -= data['hold']
 
-    add_bot(new_bot)
+        for i in trading_bots:
+            if i['name'] != data['name']:
+                i['hold'] = int(allowance / (len(trading_bots) - 1))
+
+
+    elif data['task'] == 'create':
+        name = data['name']
+        exchange_pair = data['exchange_pair']
+        hold = data['hold']
+
+        new_bot = {
+            'name': name,
+            'exchange_pair': exchange_pair,
+            'hold': hold,
+            'holds': False
+        }
+
+        add_bot(new_bot)
+
 
     return({
         'Message': "success",
         'Bots': trading_bots
     })
+
+
+@app.route('/stoploss', methods=['POST'])
+def stoploss():
+    # Load data from post
+    data = json.loads(request.data)
+
+    return ()
